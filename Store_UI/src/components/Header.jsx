192440a618fd4a557698830498cba24cd417584c
Minh Quý <mquy2810@gmail.com>
2025-10-07 11:27:31 +0700

    making header of page

--- Store_UI/src/components/Header.jsx
+++ Store_UI/src/components/Header.jsx
@@ -4,39 +4,41 @@ import {faShoppingBasket, faTags} from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
     return (
         <header className = "header">
-            <div className = "container"></div>
-            <a href = "/" className = "link">
-            <FontAwesomeIcon icon = {faTags} className = "fa-icon" />
-            <span className = "brand-title">Sticker-Store</span>
-            </a>
-            <nav className = "nav"></nav>
-            <ul>
-                <li>
-                    <a href = "/" className = "nav-link">
-                        Home
-                    </a>
-                </li>
-                <li>
-                    <a href = "/about" className = "nav-link">
-                        About
-                    </a>
-                </li>
-                <li>
-                    <a href = "/contact" className = "nav-link">
-                        Contact
-                    </a>
-                </li>
-                <li>
-                    <a href = "/login" className = "nav-link">
-                        Login
-                    </a>
-                </li>
-                <li>
-                    <a href = "/cart" className = "nav-link">
-                        <FontAwesomeIcon icon = {faShoppingBasket} /> 
-                    </a>
-                </li>
-            </ul>
+            <div className = "container">
+                <a href = "/" className = "link">
+                <FontAwesomeIcon icon = {faTags} className = "fa-icon" />
+                <span className = "brand-title">Sticker-Store</span>
+                </a>
+                <nav className = "nav">
+                    <ul>
+                        <li>
+                            <a href = "/" className = "nav-link">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href = "/about" className = "nav-link">
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a href = "/contact" className = "nav-link">
+                                Contact
+                            </a>
+                        </li>
+                        <li>
+                            <a href = "/login" className = "nav-link">
+                                Login
+                            </a>
+                        </li>
+                        <li>
+                            <a href = "/cart" className = "nav-link">
+                                <FontAwesomeIcon icon = {faShoppingBasket} /> 
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 };
