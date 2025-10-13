ac9f55aac3ab56c7ea67b1c6ac0d264a74a90643
Minh Quý <mquy2810@gmail.com>
2025-10-13 19:02:13 +0700

    update warning alert

--- Store_UI/src/components/Home.jsx
+++ Store_UI/src/components/Home.jsx
@@ -15,6 +15,18 @@ export default function Home()
                 <BootstrapButton text="Delete" type="warning" />
                 <BootstrapButton text="Link" type="link" />
             </div>
+            <div className="d-grid gap-2 col-8 mx-auto">
+                <div className="alert alert-primary text-center" role="alert"></div>
+                <div className="alert alert-warning alert-dismissible fade show" role="alert">
+                    <strong>Warning!</strong>This is a warning alert.
+                    <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="alert"
+                        aria-label="Close"
+                    ></button>
+                </div>
+            </div>
             <PageHeading  title="Explore Sticker">
                 Add a touch of creativity to your space with our wide rage of fun 
                 and unique stickers. Perfect for any occasion!
